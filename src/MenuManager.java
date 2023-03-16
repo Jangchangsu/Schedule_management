5b452e65e503f595574f035ba84c76f4de483e15
Changsu <janghyang_@naver.com>
2023-03-16 23:18:32 +0900

    added MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,8 +1,12 @@
 
-public class hello 
+public class MenuManager 
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello");
+		System.out.println("1. Add Schedule");
+		System.out.println("2. Delete Schedule");
+		System.out.println("3. Edit Schedule");
+		System.out.println("4. View Schedule");
+		System.out.println("5. Exit");
 	}
 }
